c91e6604e8e7557b71de88b2bb60b5b9406e08d1
Manish Kashyap <manishkashyap1871@gmail.com>
2026-04-07 17:06:58 +0000

    commit

---
+++ 05_iterations/two.js
@@ -0,0 +1,36 @@
+//------------------- while loop
+
+//----------------- basic syntax of while 
+// while (condition) {
+    
+// }
+
+//------------------example 1
+// let index =0
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index+=2
+// }
+
+//------------------example 2
+// let myarray = ['flash','batman','superman']
+// let arr = 0
+// while(arr<myarray.length){
+//     console.log(`value is ${myarray[arr]}`);
+//     arr+=1
+// }
+
+//--------------- do while loop
+
+//--------------basic syntax of do while loop
+// do {
+    
+// } while (condition);
+
+//--------------example 1
+
+let score = 0
+do {
+    console.log(`score is ${score}`);
+    score+=1  
+} while (score<=10);
